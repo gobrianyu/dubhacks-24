86f051917e71d75ac3a0ddddde97efec31788564
brian y. <brian127@uw.edu>
2024-10-12 14:27:12 -0700

    updates to social.dart

---
+++ dubhacks24_flutter_frontend/lib/post.dart
@@ -0,0 +1,9 @@
+class DreamPost {
+  final String username;
+  final DateTime time;
+  final profilePic;
+  final String caption;
+  // post + user pfp as images
+
+  DreamPost({required this.username, required this.time, required this.profilePic, required this.caption});
+}
