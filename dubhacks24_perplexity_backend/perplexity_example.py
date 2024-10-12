ef6775124cae099995b5b8da7c90423afa95863a
SulKhu <sul.khu@gmail.com>
2024-10-12 15:18:54 -0700

    working image generation with prompts from Flutter with Flask

--- dubhacks24_perplexity_backend/perplexity_example.py
+++ dubhacks24_perplexity_backend/perplexity_example.py
@@ -8,54 +8,25 @@ response = ''
 
 @app.route('/prompt', methods = ['GET', 'POST'])
 
+# this is our method to take in a prompt from the Flutter user that they input as a description of their dream
+# and output an image url that is a visual representation of the dream using OpenAI's DALL-E-2 image generator
+# returns an image url that the Flutter side can use to download and show the image to the user
 def promptRoute():
     global response
-
     if (request.method == 'POST'):
+        client = OpenAI()
+
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        prompt = request_data['prompt'] #extracts what user is sending, gets what they put into the form
+        prompt = request_data['prompt']
+        final_prompt = "Create an image in a dreamlike format with the described dream: " + prompt
 
-        #we need to implement some sort of code to send the user's input to the model and print
-        #out the image in return
-        return " "
-    
-        #need to run flask side on terminal to get url
-
-
-
-YOUR_API_KEY = "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"
-
-messages = [
-    {
-        "role": "system",
-        "content": (
-            "You are an artificial intelligence assistant and you need to "
-            "engage in a helpful, detailed, polite conversation with a user."
-        ),
-    },
-    {
-        "role": "user",
-        "content": (
-            "How many stars are in the universe?"
-        ),
-    },
-]
-
-client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
-
-# chat completion without streaming
-response = client.chat.completions.create(
-    model="llama-3-sonar-large-32k-online",
-    messages=messages,
-)
-print(response)
-
-# chat completion with streaming
-response_stream = client.chat.completions.create(
-    model="llama-3-sonar-large-32k-online",
-    messages=messages,
-    stream=True,
-)
-for response in response_stream:
-    print(response)
+        response = client.images.generate(
+            model="dall-e-2",
+            prompt=final_prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        image_url = response.data[0].url
+        return (image_url)
