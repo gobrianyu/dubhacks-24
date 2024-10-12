3fc07172a3988f4772ce93352f5715d69202a799
brian y. <brian127@uw.edu>
2024-10-12 16:30:23 -0700

    creaated file for diary and in process of editing account provider

--- dubhacks24_flutter_frontend/lib/account_provider.dart
+++ dubhacks24_flutter_frontend/lib/account_provider.dart
@@ -2,13 +2,22 @@ import 'package:dubhacks24_flutter_frontend/post.dart';
 import 'package:flutter/material.dart';
 
 class AccountProvider extends ChangeNotifier {
+  final DateTime accountStart; // account creation date
   final List<DreamPost> posts;
   String username;
   // final pfp;
   final List<String> followers;
   final List<String> following;
-  
-  AccountProvider({required this.username}): posts = [], followers = [], following = [];
+
+  AccountProvider({
+    required this.username,
+    required this.accountStart,
+    required this.posts,
+    required this.followers,
+    required this.following
+  });
+
+  factory AccountProvider.fromNew({required username}): accountStart = DateTime.now(), posts = [], followers = [], following = [];
 
   List<DreamPost> get myPosts => List.from(posts);
 
